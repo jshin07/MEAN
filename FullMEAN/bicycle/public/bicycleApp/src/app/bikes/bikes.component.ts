aab549732dc6f228bf7b26d3e903649ef9947e35
Jenny <jshin07@gmail.com>
2017-08-25 14:21:29 -0400

    bicycle partially completed

---
+++ FullMEAN/bicycle/public/bicycleApp/src/app/bikes/bikes.component.ts
@@ -0,0 +1,62 @@
+import {Router} from '@angular/router';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Bicycle } from './../bicycle';
+import { BicycleService } from '../bicycle.service';
+import { User } from './../user';
+
+@Component({
+  selector: 'app-bikes',
+  templateUrl: './bikes.component.html',
+  styleUrls: ['./bikes.component.css']
+})
+export class BikesComponent implements OnInit {
+  @Input() allBikes;
+  allUsers: Array<User>;
+  currentUser= {};
+  newlist;
+
+  
+  // @Output() bikeDeleted:EventEmitter<String> = new EventEmitter();
+
+  constructor(private _bikeService: BicycleService, private _router: Router) { }
+
+  ngOnInit() {
+
+    this._bikeService.currentUser()
+    .then (data => {
+      // console.log(data.error);
+      if (data.error == "not in session"){
+        this._router.navigate(['/'])
+      }
+      this.currentUser = data;
+    })
+    .catch( err => {
+      console.log(err);
+    })
+
+    this._bikeService.allUsers()
+    .then ( data => {
+      console.log(data);
+      this.allUsers = data;
+    })
+    .catch( err => {
+      console.log(err);
+    })
+
+  }
+
+
+  delete(id){
+    this._bikeService.destroy(id)
+    .then ( data => {
+      console.log(data);
+      // this.newlist= this._bikeService.allBikes();
+      // this.allBikes= this.newlist;
+    })
+    .catch (err => {
+      console.log(err)
+    })
+  }
+
+
+}
